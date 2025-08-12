668bdc0e5cb29c8a1a61f843d10a6fdeffa003d7
Lucas César Lopes <lucascesarlopes@nagazakisoftware.com.br>
2025-08-12 20:28:39 -0300

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,30 +8,11 @@ import BraintreeCore
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Registrar plugins Flutter
     GeneratedPluginRegistrant.register(with: self)
-    
-    // Configura o esquema da URL para o callback do Braintree PayPal
-    BTAppContextSwitcher.setReturnURLScheme("com.nagazakisoftware.quick.payments") // <--- coloque seu CFBundleURLSchemes aqui
-    
+    BTAppContextSwitcher.setReturnURLScheme("com.nagazakisoftware.quick.payments")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-// Configura o esquema da URL para o callback do Braintree PayPal
-  BTAppContextSwitcher.setReturnURLScheme("com.nagazakisoftware.quick.payments") // <--- coloque seu CFBundleURLSchemes aqui
-  
-  return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-}
-
-// Método para tratar a abertura da URL de callback do PayPal/Braintree
-override func application(
-  _ app: UIApplication,
-  open url: URL,
-  options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-) -> Bool {
-  if BTAppContextSwitcher.handleOpenURL(url) {
-
-  // Método para tratar a abertura da URL de callback do PayPal/Braintree
   override func application(
     _ app: UIApplication,
     open url: URL,
