b172b2e6afa13371ab1054581199b35efd65081d
Lucas César Lopes <lucascesarlopes@nagazakisoftware.com.br>
2025-08-13 12:33:46 -0300

    Remove Braintree configuration from iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,5 @@
 import UIKit
 import Flutter
-import BraintreeCore
-
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -10,22 +8,6 @@ import BraintreeCore
   ) -> Bool {
     // Registrar plugins Flutter
     GeneratedPluginRegistrant.register(with: self)
-    
-    // Configura o esquema da URL para o callback do Braintree PayPal
-    BTAppContextSwitcher.setReturnURLScheme("com.nagazakisoftware.quick.payments") // <--- coloque seu CFBundleURLSchemes aqui
-    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // Método para tratar a abertura da URL de callback do PayPal/Braintree
-  override func application(
-    _ app: UIApplication,
-    open url: URL,
-    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-  ) -> Bool {
-    if BTAppContextSwitcher.handleOpenURL(url) {
-      return true
-    }
-    return super.application(app, open: url, options: options)
   }
 }
