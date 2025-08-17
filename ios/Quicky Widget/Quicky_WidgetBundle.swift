d485a0aa03ca10ecc09a66c5a9a720f731e59972
Lucas César Lopes <lucascesarlopes@nagazakisoftware.com.br>
2025-08-17 11:48:39 -0300

    Atualizar o Quicky_WidgetBundle.swift

--- ios/Quicky Widget/Quicky_WidgetBundle.swift
+++ ios/Quicky Widget/Quicky_WidgetBundle.swift
@@ -8,10 +8,23 @@
 import WidgetKit
 import SwiftUI
 
+#if canImport(ActivityKit)
+import ActivityKit
+#endif
+
 @main
+@available(iOSApplicationExtension 16.0, *)
 struct Quicky_WidgetBundle: WidgetBundle {
+    @WidgetBundleBuilder
     var body: some Widget {
+        // Widget “normal” disponível a partir do iOS 16.0 em extensão
         Quicky_Widget()
-        Quicky_WidgetLiveActivity()
+
+        // Live Activity só se o SDK tiver ActivityKit e o SO for 16.1+
+        #if canImport(ActivityKit)
+        if #available(iOSApplicationExtension 16.1, *) {
+            Quicky_WidgetLiveActivity()
+        }
+        #endif
     }
 }
